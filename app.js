1f541bb9d66299157257c7ca601cebd0ba83bfb2
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-10-02 18:19:03 +0200

    Setup Mongoose Connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,16 +20,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useUnifiedTopology', true)
+
+mongoose.connect('mongodb://localhost/shopping-cart', err => {
+  if (err) {
+    console.log('Error In Connecting To MongoDB')
+  } else {
+    console.log('Connected Success To MongoDB')
+  }
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
