4fe13555a8f20d4f30b29694919378b9c4e6f404
magdyMohammed20 <magdymohammed37@yahoo.com>
2020-10-02 19:45:58 +0200

    Create Mongoose Model

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    imagePath: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    // Product Info Type Is Object
+    productInfo: {
+        type: {
+            storageCapacity: Number,
+            numberOfSim: String,
+            cameraResolution: Number,
+            displaySize: Number
+        },
+        required: true
+    }
+
+})
+
+export default mongoose.model('ProductSchema', productSchema)
